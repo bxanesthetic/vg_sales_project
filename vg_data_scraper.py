6dafff25860214b0715ccf413d19794788c606ad
bxanesthetic <44801850+bxanesthetic@users.noreply.github.com>
2019-03-11 01:39:12 -0400

    Update vg_data_scraper.py

--- vg_data_scraper.py
+++ vg_data_scraper.py
@@ -159,8 +159,6 @@ fails=0
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-#NEED TO SCRAPE ONLY USER SCORE AND ESRB, EVERYTHING ELSE SHOULD BE A TRY STATEMENT
-
 for i in range(0,len(meta_full_url_list)):
     url= meta_full_url_list[i]
     r = requests.get(url, headers=headers)
